fae4b4da6255207a5760f80dc45576f0b9612ac4
I-am-Skboss <98142467+I-am-Skboss@users.noreply.github.com>
2024-10-16 23:39:42 +0530

    Update views.py
    Removing unnecessary imports

--- Article site/project/proto/api/views.py
+++ Article site/project/proto/api/views.py
@@ -4,16 +4,6 @@ from .serializers import ArticleSerializer, UserSerializer
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.models import User
-# from django.shortcuts import render, HttpResponse
-# from django.http import JsonResponse
-# from rest_framework.parsers import JSONParser
-# from rest_framework.decorators import api_view
-# from rest_framework.response import Response
-# from rest_framework import status
-# from rest_framework.decorators import APIView
-# from rest_framework import generics
-# from rest_framework import mixins
-
 
 class ArticleViewSet(viewsets.ModelViewSet):
     queryset =Article.objects.all()
